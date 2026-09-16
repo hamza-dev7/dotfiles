d13b07035180e79306ae9a93b6ab0962dbb96b63
hamza-dev7 <hamza77dev@gmail.com>
2026-09-16 18:24:48 +0100

    adding update alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias zed='zeditor'
 alias ls='exa -l'
 alias cd='z'
 alias rebuild='sudo nixos-rebuild switch --impure'
+alias update='cd /home/sicko/.nixos-config & nix flake update & sudo nixos-rebuild switch --impure'
 
 # Shortcuts for common git commands
 alias ga='git add'
