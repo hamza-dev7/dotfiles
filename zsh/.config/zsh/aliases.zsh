bb16f1ff5824a68ca940e23b50c6ad080186c042
hamza-dev7 <hamza77dev@gmail.com>
2026-09-18 18:45:01 +0100

    added pyrun alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias ls='exa -l'
 alias cd='z'
 alias rebuild='sudo nixos-rebuild switch --impure'
 alias update='nix flake update & sudo nixos-rebuild switch --impure'
+alias pyrun='echo export LD_LIBRARY_PATH=\"\$NIX_LD_LIBRARY_PATH\" >> .venv/bin/activate'
 
 # Shortcuts for common git commands
 alias ga='git add'
